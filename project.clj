977dbeb614ed48d0be0862545d4bcd9a47d9eab5
Tom Hanika (sys:companion) <hanika@cs.uni-kassel.de>
2024-09-06 20:18:41 +0200

    Started dev branch

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject brunt "0.1.0"
+(defproject brunt "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "A tool for formal concept analysis based on conexp-clj."
   :url "http://example.com/FIXME"
